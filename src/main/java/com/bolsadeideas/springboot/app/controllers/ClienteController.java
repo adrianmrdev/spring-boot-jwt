9227020f454ab827aa2034c96a07e0a8a803125e
Adrian Mosquera <adrian.mosquera@goplay.gg>
2020-03-09 19:58:46 +0100

    First commit (lesson 5 complete)

---
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -0,0 +1,107 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.util.paginator.PageRender;
+
+@Controller
+@SessionAttributes("cliente")
+public class ClienteController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/listar")
+	public String listar(@RequestParam(name="page", defaultValue="0") int page, Model model) {
+		
+		Pageable pageRequest = PageRequest.of(page, 4);
+		
+		Page<Cliente> clientes = clienteService.findAll(pageRequest);
+		
+		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
+		
+		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
+		return "listar";
+	}
+
+	@GetMapping("/form")
+	public String crear(Map<String, Object> model) {
+
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
+
+		return "form";
+	}
+
+	@PostMapping("/form")
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash,
+			SessionStatus status) {
+
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";
+		}
+
+		clienteService.save(cliente);
+		status.setComplete();
+		flash.addFlashAttribute("success", "Cliente creado correctamente!");
+		return "redirect:listar";
+	}
+
+	@GetMapping("/form/{id}")
+	public String editar(@PathVariable("id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+		Cliente cliente = null;
+
+		if (id > 0) {
+			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El id del cliente no existe en base de datos!");
+			}
+		} else {
+			flash.addFlashAttribute("error", "El id del cliente no pude ser 0!");
+			return "redirect:listar";
+		}
+
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
+
+		return "form";
+	}
+
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable("id") Long id, RedirectAttributes flash) {
+
+		if (id > 0) {
+			clienteService.delete(id);
+		}
+
+		flash.addFlashAttribute("success", "Cliente eliminado correctamente!");
+
+		return "redirect:/listar";
+
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,26 @@
+/* Populate tables */
+
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (5, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (6, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (7, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (8, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (9, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (10, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (11, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (12, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (13, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (14, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (15, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (16, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (17, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (18, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (19, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (20, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (21, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (22, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (23, 'Adrián', 'Mosquera', 'adrian@mail.com', '2020-01-01');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (24, 'Carolina', 'Roo', 'carolina@mail.com', '2020-01-01');
